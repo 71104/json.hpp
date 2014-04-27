f0c1f8a8bc9062af5db699082fbfe76093f92eaf
Alberto La Rocca <a71104@gmail.com>
2014-04-27 19:47:40 +0200

    Tests

--- src/test.cpp
+++ src/test.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
 	JSON::Object<
-		JSON::Field<int, 'a'>,
+		JSON::Field<int, UNPACK("hello")>,
 		JSON::Field<double, 'b'>,
 		JSON::Field<string, 'c'>,
 		JSON::Field<JSON::Object<
@@ -17,13 +17,13 @@ int main() {
 		>>>, 'x'>
 	> o;
 
-	o.Get<'a'>();
+	o.Get<UNPACK("hello")>();
 	o.Get<'x'>();
 	o.Get<'o'>().Get<'f'>();
 
 	JSON::Object<JSON::Field<bool, 'f'>>().Get<'f'>();
 
-	JSON::Field<bool, UNPACK("hello")>();
+	cout << JSON::Field<bool, UNPACK("hello")>::s_szName << endl;
 
 	return 0;
 }
